c6ca75e329781a70245cb3d23b0ada13e2c04887
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-04-19 22:17:26 +0200

    fix(database): changed all pk to id instead of id_xxx

--- api/database/dbinit.sql
+++ api/database/dbinit.sql
@@ -1,45 +1,45 @@
 CREATE TABLE IF NOT EXISTS "users"
 (
-    id_user INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "companies"
 (
-    id_company INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "users_companies"
 (
-    id_user_company INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     id_user INTEGER NOT NULL,
     id_company INTEGER NOT NULL,
-    FOREIGN KEY(id_user) REFERENCES users(id_user),
-    FOREIGN KEY(id_company) REFERENCES companies(id_company)
+    FOREIGN KEY(id_user) REFERENCES users(id),
+    FOREIGN KEY(id_company) REFERENCES companies(id)
 );
 
 CREATE TABLE IF NOT EXISTS "warehouses"
 (
-    id_warehouse INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL,
     id_company INTEGER NOT NULL,
-    FOREIGN KEY(id_company) REFERENCES companies(id_company)
+    FOREIGN KEY(id_company) REFERENCES companies(id)
 );
 
 CREATE TABLE IF NOT EXISTS "companies_warehouses"
 (
-    id_company_warehouse INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     id_company INTEGER NOT NULL,
     id_warehouse INTEGER NOT NULL,
-    FOREIGN KEY(id_company) REFERENCES companies(id_company),
-    FOREIGN KEY(id_warehouse) REFERENCES warehouses(id_warehouse)
+    FOREIGN KEY(id_company) REFERENCES companies(id),
+    FOREIGN KEY(id_warehouse) REFERENCES warehouses(id)
 );
 
 CREATE TABLE IF NOT EXISTS "items_references"
 (
-    id_reference INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     barcode_value INTEGER NOT NULL,
     name TEXT NOT NULL,
     price REAL NOT NULL
@@ -47,47 +47,47 @@ CREATE TABLE IF NOT EXISTS "items_references"
 
 CREATE TABLE IF NOT EXISTS "companies_references"
 (
-    id_company_reference INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     id_company INTEGER NOT NULL,
     id_reference INTEGER NOT NULL,
-    FOREIGN KEY(id_company) REFERENCES companies(id_company),
-    FOREIGN KEY(id_reference) REFERENCES items_references(id_reference)
+    FOREIGN KEY(id_company) REFERENCES companies(id),
+    FOREIGN KEY(id_reference) REFERENCES items_references(id)
 );
 
 CREATE TABLE IF NOT EXISTS "articles"
 (
-    id_article INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     id_reference INTEGER NOT NULL,
     expiration TEXT,
-    FOREIGN KEY(id_reference) REFERENCES items_references(id_reference)
+    FOREIGN KEY(id_reference) REFERENCES items_references(id)
 );
 
 CREATE TABLE IF NOT EXISTS "sales"
 (
-    id_sale INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     created_at TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "sales_items"
 (
-    id_sale_item INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     id_sale INTEGER NOT NULL,
     id_article INTEGER NOT NULL,
-    FOREIGN KEY(id_sale) REFERENCES sales(id_sale),
-    FOREIGN KEY(id_article) REFERENCES articles(id_article)
+    FOREIGN KEY(id_sale) REFERENCES sales(id),
+    FOREIGN KEY(id_article) REFERENCES articles(id)
 );
 
 CREATE TABLE IF NOT EXISTS "orders"
 (
-    id_order INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     created_at TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "orders_items"
 (
-    id_order_item INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
     id_order INTEGER NOT NULL,
     id_reference INTEGER NOT NULL,
-    FOREIGN KEY(id_order) REFERENCES orders(id_order),
-    FOREIGN KEY(id_reference) REFERENCES items_references(id_reference)
+    FOREIGN KEY(id_order) REFERENCES orders(id),
+    FOREIGN KEY(id_reference) REFERENCES items_references(id)
 );
