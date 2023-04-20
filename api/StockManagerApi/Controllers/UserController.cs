beb2c7dc5e8a9f5261905a94b1a47158cdb824ad
Alexis Provo <alexis.provo@ynov.com>
2023-04-20 11:04:33 +0200

    feat(api): add register endpoint

---
+++ api/StockManagerApi/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManagerApi.Data;
+using StockManagerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username == username))
+            {
+                return BadRequest(new { message = "Username already exists" });
+            }
+
+            var user = new User
+            {
+                Username = username,
+                Password = BCrypt.Net.BCrypt.HashPassword(password)
+            };
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Registration succeeded." });
+        }
+    }
+}
