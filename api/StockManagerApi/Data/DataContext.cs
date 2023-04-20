c4ec837f7946079ddd73735cf3476aa3733d52dd
Alexis Provo <alexis.provo@ynov.com>
2023-04-20 10:59:32 +0200

    feat(api): add User model

--- api/StockManagerApi/Data/DataContext.cs
+++ api/StockManagerApi/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StockManagerApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,10 @@ namespace StockManagerApi.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
     }
 }
