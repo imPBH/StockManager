7c3c81d3938eb8bf3be96e03e6f8246854126846
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-04-20 17:43:22 +0200

    feat(api): delete a company

--- api/StockManagerApi/Controllers/CompanyController.cs
+++ api/StockManagerApi/Controllers/CompanyController.cs
@@ -51,5 +51,37 @@ namespace StockManagerApi.Controllers
 
             return Created($"api/company/{company.Id}", company.Id);
         }
+
+        [Authorize]
+        [HttpPost("delete")]
+        public IActionResult Delete(int companyId)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == User.Identity.Name);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            var company = _context.Companies.FirstOrDefault(c => c.Id == companyId);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            var userCompany = _context.Users_Companies.FirstOrDefault(uc => uc.Id_User == user.Id && uc.Id_Company == company.Id);
+            if (userCompany == null)
+            {
+                return Forbid();
+            }
+
+            var userCompanies = _context.Users_Companies.Where(uc => uc.Id_Company == companyId);
+            _context.Users_Companies.RemoveRange(userCompanies);
+
+            _context.Companies.Remove(company);
+
+            _context.SaveChanges();
+
+            return Ok(new { message = "Successfully deleted" });
+        }
     }
 }
