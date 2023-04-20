b73ab8a4685cb991d1f86f27399812f0b73a66ba
Alexis Provo <alexis.provo@ynov.com>
2023-04-20 10:53:44 +0200

    feat(api): add connection to the database

--- api/StockManagerApi/Startup.cs
+++ api/StockManagerApi/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StockManagerApi.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,8 @@ namespace StockManagerApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StockManagerApi", Version = "v1" });
             });
+            services.AddDbContext<DataContext>(x => x.UseSqlite
+            (Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
