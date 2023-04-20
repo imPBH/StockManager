d25e01c025f1e7022a484379088a1f422648e2cf
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-04-20 17:40:21 +0200

    feat(api): add company and user_company models

---
+++ api/StockManagerApi/Models/Company.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace StockManagerApi.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ api/StockManagerApi/Models/UserCompany.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManagerApi.Models
+{
+    public class UserCompany
+    {
+        public int Id { get; set; }
+        public int Id_User { get; set; }
+        public int Id_Company { get; set; }
+
+        // Relation avec la table Companies
+        [ForeignKey("Id_Company")]
+        public Company Company { get; set; }
+
+        [ForeignKey("Id_User")]
+        public Company User { get; set; }
+    }
+}
