17c1b730f0fcf69678165608b613157cefe9fa96
Alexis Provo <alexis.provo@ynov.com>
2023-04-21 10:29:19 +0200

    feat(api): add a new warehouse

---
+++ api/StockManagerApi/Controllers/WarehouseController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManagerApi.Data;
+using StockManagerApi.Models;
+using System.ComponentModel.Design;
+using System.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace StockManagerApi.Controllers
+{
+    [Route("api/warehouses")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public WarehouseController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpPost("create")]
+        public IActionResult Create(string name, int companyId)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == User.Identity.Name);
+            if (user == null)
+            {
+                return StatusCode(401);
+            }
+
+            var company = _context.Companies.FirstOrDefault(c => c.Id == companyId);
+            if (company == null)
+            {
+                return BadRequest(new { message = "Company not found" });
+            }
+
+            var userCompany = _context.Users_Companies.FirstOrDefault(uc => uc.Id_User == user.Id && uc.Id_Company == company.Id);
+            if (userCompany == null)
+            {
+                return Forbid();
+            }
+
+            var warehouse = new Warehouse
+            {
+                Name = name,
+                Id_Company = companyId
+            };
+
+            _context.Warehouses.Add(warehouse);
+
+            _context.SaveChanges();
+
+            return Created(name, companyId);
+
+        }
+    }
+}
