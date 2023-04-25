84eb7041f865a86f9c1bce5de75599568c3ae5c9
Alexis Provo <alexis.provo@ynov.com>
2023-04-25 16:23:10 +0200

    feat(api): added CompanyReference and ItemReference to data context

---
+++ api/StockManagerApi/Models/CompanyReference.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManagerApi.Models
+{
+    public class CompanyReference
+    {
+        public int Id { get; set; }
+        public int Id_Company { get; set; }
+        public int Id_Reference { get; set; }
+
+        [ForeignKey("Id_Company")]
+        public Company Company { get; set; }
+
+        [ForeignKey("Id_Reference")]
+        public ItemReference Reference { get; set; }
+
+    }
+}
---
+++ api/StockManagerApi/Models/ItemReference.cs
@@ -0,0 +1,10 @@
+﻿namespace StockManagerApi.Models
+{
+    public class ItemReference
+    {
+        public int Id { get; set; }
+        public int Barcode_value { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
