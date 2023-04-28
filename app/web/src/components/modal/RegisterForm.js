c4ceb7b958b6a9f253c3342054e3ba4f325a796c
Kasoro47 <kilian.rosak@ynov.com>
2023-04-28 14:00:24 +0200

    feat(web): created component RegisterForm

---
+++ app/web/src/components/modal/RegisterForm.js
@@ -0,0 +1,140 @@
+import { useState } from "react";
+
+export default function RegisterForm({ onSubmit }) {
+  const [showModal, setShowModal] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+  
+    if (formJson.password !== formJson.retype_password) {
+      setPasswordError(true);
+      return;
+    } else {
+      setPasswordError(false);
+    }
+  
+    onSubmit(formJson);
+    closeModal();
+  }
+  
+
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <button
+          onClick={openModal}
+          className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          type="button"
+        >
+          Register
+        </button>
+      </div>
+      {showModal && (
+        <div
+          onClick={closeModal}
+          className="fixed top-0 left-0 right-0 bottom-0 bg-gray-700 bg-opacity-50 z-50"
+        >
+          <div
+            onClick={(event) => event.stopPropagation()}
+            className="relative bg-white rounded-lg shadow w-full max-w-md mx-auto my-8 p-4 md:p-6"
+          >
+            <button
+              onClick={closeModal}
+              type="button"
+              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-black"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="px-6 py-6 lg:px-8">
+              <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-black">
+                Create company
+              </h3>
+              <form className="space-y-6" method="post" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                  >
+                    Your username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Name"
+                    required
+                  />
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                  >
+                    Your password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Password"
+                    required
+                  />
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                  >
+                    Re-type password
+                  </label>
+                  <input
+                    type="password"
+                    name="retype_password"
+                    id="retype_password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Re-type Password"
+                    required
+                    />
+                  {passwordError && (
+                  <p className="text-red-600 text-sm mt-1">
+                    Passwords do not match. Please try again.
+                  </p>
+                )}
+                  <div className="flex justify-center items-center">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
+                      Create
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )
+    }
+    </>
+)}
