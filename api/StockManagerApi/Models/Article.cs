8fb2d0a8028cf069e4ed875805349d5322336dec
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-05-02 23:05:49 +0200

    feat(api): add new article (#15)
    * fix(database): removed companies_warehouses table (useless) and added id_warehouse for articles
    
    * feat(api): created Article model
    
    * feat(api): create a new article

---
+++ api/StockManagerApi/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockManagerApi.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public int Id_Reference { get; set; }
+        public int Id_Warehouse { get; set; }
+        public DateTime? Expiration { get; set; }
+    }
+}
