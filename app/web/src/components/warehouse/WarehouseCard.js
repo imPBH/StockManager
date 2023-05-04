cc06c0f9ff900437d22fa2da3b8cd8d0ce7c2c54
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-05-04 02:16:46 +0200

    feat(web): Warehouse card

---
+++ app/web/src/components/warehouse/WarehouseCard.js
@@ -0,0 +1,30 @@
+export default function WarehouseCard({warehouse}) {  
+    return (
+      <>
+          <div className="pt-5 pr-0 pb-0 pl-0 mt-5 mr-0 mb-0 ml-0">
+            <div className="sm:flex sm:items-center sm:justify-between sm:space-x-5">
+              <div className="flex items-center flex-1 min-w-0">
+                <img
+                  src={warehouse.logo}
+                  alt={warehouse.name}
+                  className="flex-shrink-0 object-cover rounded-full btn- w-10 h-10"
+                />
+                <div className="mt-0 mr-0 mb-0 ml-4 flex-1 min-w-0">
+                  <p className="text-lg font-bold text-gray-800 truncate">
+                    {warehouse.name}
+                  </p>
+                </div>
+              </div>
+              <div className="mt-4 mr-0 mb-0 ml-0 pt-0 pr-0 pb-0 pl-14 flex items-center sm:space-x-6 sm:pl-0 sm:mt-0">
+                <a
+                  href="#"
+                  className="bg-gray-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all duration-200 hover:bg-gray-700 rounded-lg"
+                >
+                  Choose
+                </a>
+              </div>
+            </div>
+          </div>
+      </>
+    );
+  }
