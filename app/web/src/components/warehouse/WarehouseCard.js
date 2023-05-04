d7877e8fca5b949d7470b01be41d579e3a2c81e5
Alexis Provo <alexis.provo@ynov.com>
2023-05-04 13:13:47 +0200

    feat(web): add article to next router

--- app/web/src/components/warehouse/WarehouseCard.js
+++ app/web/src/components/warehouse/WarehouseCard.js
@@ -17,10 +17,16 @@ export default function WarehouseCard({warehouse}) {
               </div>
               <div className="mt-4 mr-0 mb-0 ml-0 pt-0 pr-0 pb-0 pl-14 flex items-center sm:space-x-6 sm:pl-0 sm:mt-0">
                 <a
+                  href={"/articles/"+warehouse.id}
+                  className="bg-gray-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all duration-200 hover:bg-gray-700 rounded-lg"
+                >
+                  Articles
+                </a>
+                <a
                   href="#"
                   className="bg-gray-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all duration-200 hover:bg-gray-700 rounded-lg"
                 >
-                  Choose
+                  Edit
                 </a>
               </div>
             </div>
