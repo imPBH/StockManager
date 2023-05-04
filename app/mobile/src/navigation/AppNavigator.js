4ead591855f0a29dd3fe21c320ce79c741349d05
Alexis Provo <alexis.provo@ynov.com>
2023-05-04 16:22:40 +0200

    feat(app): redirect to new reference screen if not exists

--- app/mobile/src/navigation/AppNavigator.js
+++ app/mobile/src/navigation/AppNavigator.js
@@ -5,6 +5,7 @@ import WarehouseSelectionScreen from '../screens/WarehouseSelectionScreen';
 import PrincipalScreen from '../screens/PrincipalScreen.js';
 import ManageInventoryScreen from '../screens/ManageInventoryScreen';
 import SaleScreen from '../screens/SaleScreen.js';
+import NewReferenceScreen from '../screens/NewReferenceScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,7 @@ export default function AppNavigator() {
       <Stack.Screen name="PrincipalScreen" component={PrincipalScreen} />
       <Stack.Screen name="ManageInventoryScreen" component={ManageInventoryScreen} />
       <Stack.Screen name="SaleScreen" component={SaleScreen} />
+      <Stack.Screen name="NewReferenceScreen" component={NewReferenceScreen} />
 
     </Stack.Navigator>
   );
