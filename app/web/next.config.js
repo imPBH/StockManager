b1bd3adbc7b4fb253305ae3bfbd42095cbdda185
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-05-04 02:16:23 +0200

    feat(web): use router for warehouses/companyId

--- app/web/next.config.js
+++ app/web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/warehouses/:idCompany',
+        destination: '/warehouses',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
