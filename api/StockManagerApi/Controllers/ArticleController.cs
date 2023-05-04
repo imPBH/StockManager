5d977ea8735a11e3db0935610a4c13e6a213c8f6
Alexis Provo <59230262+imPBH@users.noreply.github.com>
2023-05-04 11:32:18 +0200

    feat(api): get warehouse articles (#27)

--- api/StockManagerApi/Controllers/ArticleController.cs
+++ api/StockManagerApi/Controllers/ArticleController.cs
@@ -21,9 +21,41 @@ namespace StockManagerApi.Controllers
 
         [Authorize]
         [HttpPost("create")]
-        public IActionResult Create(int idReference, DateTime? expiration, int idWarehouse)
+        public IActionResult Create(Article article)
         {
+            var user = _context.Users.FirstOrDefault(u => u.Username == User.Identity.Name);
+            if (user == null)
+            {
+                return StatusCode(401);
+            }
 
+            var warehouse = _context.Warehouses.FirstOrDefault(warehouse => warehouse.Id == article.Id_Warehouse);
+            if (warehouse == null)
+            {
+                return BadRequest("Warehouse not found");
+            }
+
+            var userCompany = _context.Users_Companies.FirstOrDefault(uc => uc.Id_User == user.Id && uc.Id_Company == warehouse.Id_Company);
+            if (userCompany == null)
+            {
+                return Forbid();
+            }
+
+            var companyReference = _context.Companies_References.FirstOrDefault(cr => cr.Id_Company == warehouse.Id_Company && cr.Id_Reference == article.Id_Reference);
+            if (companyReference == null)
+            {
+                return Forbid();
+            }
+
+            _context.Articles.Add(article);
+            _context.SaveChanges();
+            return Created($"api/article/{article.Id}", article.Id);
+        }
+
+        [Authorize]
+        [HttpGet("get")]
+        public IActionResult Get(int idWarehouse)
+        {
             var user = _context.Users.FirstOrDefault(u => u.Username == User.Identity.Name);
             if (user == null)
             {
@@ -42,22 +74,8 @@ namespace StockManagerApi.Controllers
                 return Forbid();
             }
 
-            var companyReference = _context.Companies_References.FirstOrDefault(cr => cr.Id_Company == warehouse.Id_Company && cr.Id_Reference == idReference);
-            if (companyReference == null)
-            {
-                return Forbid();
-            }
-
-            var newArticle = new Article
-            {
-                Id_Reference = idReference,
-                Id_Warehouse = idWarehouse,
-                Expiration = expiration
-            };
-
-            _context.Articles.Add(newArticle);
-            _context.SaveChanges();
-            return Created($"api/article/{newArticle.Id}", newArticle.Id);
+            var articles = _context.Articles.Where(a => a.Id_Warehouse == idWarehouse).ToList();
+            return Ok(articles);
         }
     }
 }
