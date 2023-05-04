1561e6ea148f064ddb32dded5605b3a07f10b7e2
MENOU LUCAS <91542549+nos193@users.noreply.github.com>
2023-05-04 15:19:55 +0200

    feat(app): scan barcode (#30)

---
+++ app/mobile/src/screens/ManageInventoryScreen.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function ScannerScreenAdd() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Code-barres de type ${type} et de données ${data} a été ajouté!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Demande de permission pour utiliser la caméra</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Pas d'accès à la caméra</Text>;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Scanner à nouveau'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    title: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+  });
